714cb0f1bccf22ccadb7a563975bdb82fc4c831f
Aldi Nugraha <aldi.nugraha@gmail.com>
2022-06-17 13:55:12 +0700

    CRUD Article

--- resources/views/pages/admin/article/index.blade.php
+++ resources/views/pages/admin/article/index.blade.php
@@ -5,7 +5,7 @@
 
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    <h1 class="h3 mb-0 text-gray-800">Artikel</h1>
     <a href="{{ route('article.create') }}" class="btn btn-primary shadow-sm">
       <i class="fas fa-plus fa-sm text-white"></i> Tambah Artikel
     </a>
