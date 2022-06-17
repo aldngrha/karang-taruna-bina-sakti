622a9aad63515246186e5668e9bb56deec481825
Aldi Nugraha <aldi.nugraha@gmail.com>
2022-06-17 14:07:54 +0700

    Adding route gallery

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -42,7 +42,7 @@
 
   <!-- Nav Item - Tables -->
   <li class="nav-item">
-    <a class="nav-link" href="tables.html">
+    <a class="nav-link" href="{{ route('gallery.index') }}">
       <i class="fas fa-images"></i>
       <span>Galeri</span></a>
   </li>
