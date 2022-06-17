0d9e5c7abe4ec7f681d457f31db2b89a201db058
Aldi Nugraha <aldi.nugraha@gmail.com>
2022-06-17 13:40:56 +0700

    CRUD Slider

--- resources/views/pages/admin/slider/edit.blade.php
+++ resources/views/pages/admin/slider/edit.blade.php
@@ -9,7 +9,7 @@ Ubah Gallery
 <div class="container-fluid">
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Ubah Slider</h1>
+    <h1 class="h3 mb-0 text-gray-800">Ubah Slider {{ $item->id }}</h1>
   </div>
 
   @if ($errors->any())
