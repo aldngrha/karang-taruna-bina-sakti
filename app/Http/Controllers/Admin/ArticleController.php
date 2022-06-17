41d99fe4f523b54e5dfd1fa59803a25e306bb1ce
Aldi Nugraha <aldi.nugraha@gmail.com>
2022-06-17 14:04:26 +0700

    CRUD Article

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -110,6 +110,6 @@ class ArticleController extends Controller
         $item = Article::findOrFail($id);
         $item->delete();
 
-        return redirect()->route("article.index");
+        return redirect()->route("article");
     }
 }
