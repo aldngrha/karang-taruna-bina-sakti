23d99e2cffccb01689b834993ca6c27704c24ca7
Aldi Nugraha <aldi.nugraha@gmail.com>
2022-06-17 13:18:10 +0700

    CRUD Slider

--- resources/views/pages/admin/slider/index.blade.php
+++ resources/views/pages/admin/slider/index.blade.php
@@ -7,7 +7,7 @@
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     <a href="{{ route('slider.create') }}" class="btn btn-primary shadow-sm">
-      <i class="fas fa-plus fa-sm text-white"></i> Tambah Artikel
+      <i class="fas fa-plus fa-sm text-white"></i> Tambah Slider
     </a>
   </div>
 
